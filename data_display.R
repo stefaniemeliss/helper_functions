8c1cd883fcef8d50658b934c413cd63df79d45d3
Stefanie Meliss, PhD <55539825+stefaniemeliss@users.noreply.github.com>
2025-03-11 13:34:23 +0000

    move function to grouped related functions

---
+++ data_display.R
@@ -0,0 +1,18 @@
+# functions for data display #
+
+table_desc <- function(data = df, group_var = "group", dep_var = "variable"){
+  
+  out <- rbind(
+    psych::describe(data[, dep_var]), # get descriptives whole sample
+    do.call("rbind",psych::describeBy(data[, dep_var], group = data[, group_var])) # get descriptives per group
+  )
+  # edit output
+  out$vars <- NULL
+  rownames(out)[1] <- "all"
+  out <- round(out, 3)
+  # print output
+  kbl(out, caption = paste0("Descriptives of variable '", dep_var,"' for whole sample and within each group")) %>%
+    kable_styling(bootstrap_options = c("striped", "hover", "condensed"), fixed_thead = T) %>% 
+    print()
+  cat("\n")
+}
