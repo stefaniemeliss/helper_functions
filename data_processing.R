7fc18b8ae650b149630becbae6a4b928e3a40df4
Stefanie Meliss, PhD <55539825+stefaniemeliss@users.noreply.github.com>
2025-03-11 13:32:37 +0000

    move function to grouped related functions

---
+++ data_processing.R
@@ -0,0 +1,21 @@
+# functions for data processing #
+
+# function to determine outliers
+is_outlier_iqr <- function(x) {
+  # +/- 1.5*IQR
+  return(x < quantile(x, 0.25, na.rm = T) - 1.5 * IQR(x, na.rm = T) | x > quantile(x, 0.75, na.rm = T) + 1.5 * IQR(x, na.rm = T))
+}
+
+# rbind all columns
+rbind.all.columns <- function(x, y) {
+  
+  x.diff <- setdiff(colnames(x), colnames(y))
+  y.diff <- setdiff(colnames(y), colnames(x))
+  
+  x[, c(as.character(y.diff))] <- NA
+  
+  y[, c(as.character(x.diff))] <- NA
+  
+  return(rbind(x, y))
+}
+
--- functions.R
+++ functions.R
@@ -1,24 +1,3 @@
-# functions for data processing #
-
-# function to determine outliers
-is_outlier_iqr <- function(x) {
-  # +/- 1.5*IQR
-  return(x < quantile(x, 0.25, na.rm = T) - 1.5 * IQR(x, na.rm = T) | x > quantile(x, 0.75, na.rm = T) + 1.5 * IQR(x, na.rm = T))
-}
-
-# rbind all columns
-rbind.all.columns <- function(x, y) {
-  
-  x.diff <- setdiff(colnames(x), colnames(y))
-  y.diff <- setdiff(colnames(y), colnames(x))
-  
-  x[, c(as.character(y.diff))] <- NA
-  
-  y[, c(as.character(x.diff))] <- NA
-  
-  return(rbind(x, y))
-}
-
 # functions for data display #
 
 table_desc <- function(data = df, group_var = "group", dep_var = "variable"){
