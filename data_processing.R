fff5cb852c39d26c7266829004f8b23035b68c42
Stefanie Meliss, PhD <55539825+stefaniemeliss@users.noreply.github.com>
2025-03-17 16:13:24 +0000

    add function to compute outliers using the mean +/- 3*SD rule

--- data_processing.R
+++ data_processing.R
@@ -6,6 +6,11 @@ is_outlier_iqr <- function(x) {
   return(x < quantile(x, 0.25, na.rm = T) - 1.5 * IQR(x, na.rm = T) | x > quantile(x, 0.75, na.rm = T) + 1.5 * IQR(x, na.rm = T))
 }
 
+is_outlier_3sd <- function(x) {
+  # +/- 1.5*IQR
+  return(x < mean(x, na.rm = T) - 3 * sd(x, na.rm = T) | x > mean(x, na.rm = T) + 3 * sd(x, na.rm = T))
+}
+
 # rbind all columns
 rbind.all.columns <- function(x, y) {
   
