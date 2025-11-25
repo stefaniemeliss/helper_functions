1470b5ff89ab29e15a5d81c2ac0984467123a721
Stefanie Meliss, PhD <55539825+stefaniemeliss@users.noreply.github.com>
2025-11-25 14:59:53 +0000

    add function to extract the name and directory of an R/Rmd file

--- utilities.R
+++ utilities.R
@@ -26,6 +26,36 @@ get_file_stem <- function() {
   return(file_stem)
 }
 
+# Function to extract the name and directory of an R/Rmd file
+get_file_info <- function() {
+  # Check if running in an R Markdown document
+  if (!is.null(knitr::current_input())) {
+    file_path <- knitr::current_input()
+  } else {
+    # Attempt to retrieve the script name from the commandArgs
+    args <- commandArgs(trailingOnly = FALSE)
+    script_path <- sub("--file=", "", args[grep("--file=", args)])
+    if (length(script_path) > 0) {
+      file_path <- script_path
+    } else {
+      # Fallback for interactive execution
+      file_path <- rstudioapi::getSourceEditorContext()$path
+      if (file_path == "") {
+        return(NULL)
+      }
+    }
+  }
+  
+  # Extract the base name and directory of the file
+  base_name <- basename(file_path)
+  file_stem <- tools::file_path_sans_ext(base_name)
+  file_dir <- dirname(file_path)
+  
+  # Export the base name and directory of the file to the global environment
+  assign("file_stem", file_stem, envir = .GlobalEnv)
+  assign("file_dir", file_dir, envir = .GlobalEnv)
+}
+
 # Function to dynamically identify the root directory and the subdirectory one level below
 get_directory <- function(root_dir_name = "code") {
   # Get the current working directory
